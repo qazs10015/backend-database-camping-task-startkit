a7282ec23f0b8b6ea136b17d60315892e5a30457
Junyu <happystyle666@gmail.com>
2024-12-11 21:49:56 +0800

    add colon

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -25,7 +25,7 @@ insert into "USER" ("name","email","role") VALUES('透明人','opacity0@hexschoo
 -- 1-2 修改：用 Email 找到 李燕容、肌肉棒子、Q太郎，如果他的 Role 為 USER 將他的 Role 改為 COACH
 UPDATE "USER" SET 
 "role"='COACH'
- WHERE email IN ('lee2000@hexschooltest.io','muscle@hexschooltest.io','starplatinum@hexschooltest.io')
+ WHERE email IN ('lee2000@hexschooltest.io','muscle@hexschooltest.io','starplatinum@hexschooltest.io');
 -- 1-3 刪除：刪除USER 資料表中，用 Email 找到透明人，並刪除該筆資料
  DELETE FROM "USER" WHERE email ='opacity0@hexschooltest.io';
 -- 1-4 查詢：取得USER 資料表目前所有用戶數量（提示：使用count函式）
